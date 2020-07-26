d200042c13aacfcf355ce6004e24d6b7630594d5
Sathurshan <jsathu07@gmail.com>
2020-07-26 14:00:28 +0530

    some major changes made in navigation

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Icon } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as firebase from 'firebase';
 
 import Home from './src/screens/Home.js';
-import LeaderBoard from './src/screens/LeaderBoard.js'
 
-const Tab = createBottomTabNavigator();
-// app edited my mini
+const Stack = createStackNavigator();
+
 const firebaseConfig = {
   apiKey: "AIzaSyCcETA2j0jQwMy2pvGizaBm_m6o0VJvMnA",
   authDomain: "educhange-2630e.firebaseapp.com",
@@ -26,32 +25,9 @@ export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size }) => {
-              let iconName;
-              if (route.name === "Home") {
-                iconName = "home";
-              }
-              else if (route.name === "LeaderBoard") {
-                iconName = "list";
-              }
-              else {
-                iconName = "list";
-              }
-              return <Icon name={iconName} type="FontAwesome" style={{color:"white"}}/>
-            }
-          })}
-          tabBarOptions={{
-            activeTintColor: "#74B9FF",
-            style:{
-              backgroundColor:"#000000",
-              borderTopColor:"black"
-            }
-          }}>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="LeaderBoard" component={LeaderBoard}/>
-        </Tab.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{headerShown:false}}/>
+        </Stack.Navigator>
       </NavigationContainer>
     )
   }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,23 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Icon } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './HomeScreen.js';
+import LeaderBoardScreen from './LeaderBoardScreen.js';
+
+const Tab = createBottomTabNavigator();
 
 export default class Home extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>EduChange</Text>
-                <StatusBar style="dark" />
-            </View>
-        );
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ color, size }) => {
+                            let iconName;
+                            if (route.name === "Learn") {
+                                iconName = "book";
+                            }
+                            else if (route.name === "LeaderBoard") {
+                                iconName = "list-alt";
+                            }
+                            else {
+                                iconName = "list";
+                            }
+                            return <Icon name={iconName} type="FontAwesome" style={{ color: "white" }} />
+                        }
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: "#74B9FF",
+                        style: {
+                            backgroundColor: "#000000",
+                            borderTopColor: "black"
+                        }
+                    }}>
+                    <Tab.Screen name="Learn" component={HomeScreen} />
+                    <Tab.Screen name="LeaderBoard" component={LeaderBoardScreen} />
+                </Tab.Navigator>
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
